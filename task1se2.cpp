479f04e7ca44a6926968e2bc84c5c1de1ca51072
habibahassanm <habibamohiy010@gmail.com>
2025-07-26 17:31:43 +0300

    Add files via upload

---
+++ task1se2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+   int num ;
+   cout <<"please enter the num "<< endl;
+   cin>>num;
+   cout << "++"<<num<< " = "<<++num << endl;
+   cout << num <<" ++  ="<< num++ << endl ;
+    cout << "--"<<num<< " = "<<--num << endl;
+   cout << num <<" --  ="<< num-- << endl ;
+   
+return 0 ;
+
+}
